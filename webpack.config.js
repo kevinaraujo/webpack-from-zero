d49c78f46f54580e5c3fb40217525534307f43e8
Kevin Araújo <kfa_34@hotmail.com>
2022-03-18 09:21:49 -0300

    Added react to the project and build command.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,27 @@
+const htmlWebPackPlugin = require('html-webpack-plugin')
+
 module.exports = {
     module: {
         rules: [
             {
-                test: /\.(.js|jsx)$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader"
                 }
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: "html-loader"
+                }
             }
         ]
-    }
+    },
+    plugins: [
+        new htmlWebPackPlugin({
+            "template": "./public/index.html",
+            "filename": "index.html"
+        })
+    ]
 }
